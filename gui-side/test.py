383d88c9209d2b7cd9ed6f258eef8a3b433cc72b
lufermy <luisfernandezmartinezz@gmail.com>
2023-09-09 04:43:59 +0200

    Modified settings.json and test.py for testing purposes.

--- gui-side/test.py
+++ gui-side/test.py
@@ -1,6 +1,17 @@
-#test-file
-import json
-f = open('settings.json')
-data = json.load(f)
-print (data["username"])
-f.close()
+#test-file# WORKING CODE!
+import pyodbc 
+print(pyodbc.drivers())  
+cnxn = pyodbc.connect(DRIVER='{ODBC Driver 18 for SQL Server}',
+                      Server='188.165.239.21',
+                      Database='AOR',
+                      Port='1433',
+                      UID='userAOR',
+                      PWD='AORAOR01*',
+                      TrustServerCertificate='yes')
+
+cursor = cnxn.cursor()
+cursor.execute("SELECT * FROM dbo.Tokens") 
+row = cursor.fetchone() 
+while row:
+    print (row) 
+    row = cursor.fetchone()
